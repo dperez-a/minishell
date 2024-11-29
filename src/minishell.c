8745367e04c028a654ce84b66da4d985e41ebf1f
Daniel Pérez <dani_mm__@Air-de-Daniel-2.home>
2024-11-29 10:03:08 +0100

    makefile, and msh (.c,.h) modified)

--- src/minishell.c
+++ src/minishell.c
@@ -1,6 +1,41 @@
 #include "../inc/minishell.h"
 
-int main(void)
+void test_tokenize_input(const char *input)
 {
-	return 0;
+    printf("Input: %s\n", input);
+    t_token *tokens = tokenize_input(input);
+    print_tokens(tokens);
+    // Free tokens after use
+    t_token *tmp;
+    while (tokens)
+    {
+        tmp = tokens;
+        tokens = tokens->next;
+        free(tmp->value);
+        free(tmp);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    // Test cases
+    const char *inputs[] =
+    {
+        "echo $USER | grep 'hello world' > output.txt",
+        "ls -la | grep minishell > result.txt",
+        "cat < input.txt | sort > sorted.txt",
+        "echo 'Hello, World!' >> greetings.txt",
+        "export PATH=$PATH:/new/path",
+        NULL
+    };
+
+    int i = 0;
+    while (inputs[i] != NULL)
+    {
+        test_tokenize_input(inputs[i]);
+        i++;
+    }
+
+    return 0;
 }
