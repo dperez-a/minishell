e1912921d7c6b7955a3599bf05ec34f389c10472
dperez-a <dpereza0503@gmail.com>
2024-12-18 12:57:21 +0100

    changed name of both files

--- inc/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-# define MAIN_H
-# include "../minishell.h"
-// 〖─◇──◇──◇──◇──◇──◇──◇──◇──◇──◇──◇──◇──◇│◇──◇──◇──◇──◇──◇──◇──◇──◇──◇──◇──◇─〗
-//									   〘MAIN〙
-//? main.c
-void print_tokens(t_token *tokens);
-
-#endif
